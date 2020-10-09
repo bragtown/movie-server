f4c76afc0e935b7c48a9756d4720fcdd660d8646
lostscrolls <lostscrolls@churchofjesuschrist.org>
2020-10-08 19:45:47 -0600

    serve static

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { AppService } from './app.service';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
 import { MoviesModule } from './movies/movies.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [MoviesModule],
-  controllers: [AppController, MoviesController],
-  providers: [AppService, MoviesService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/build'),
+    }),
+    MoviesModule],
+  controllers: [MoviesController],
+  providers: [MoviesService],
 })
 export class AppModule {}
