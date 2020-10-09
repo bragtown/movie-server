ab163169517396703aa59af43a51615066a06fe1
lostscrolls <lostscrolls@churchofjesuschrist.org>
2020-10-08 19:46:49 -0600

    client

---
+++ client/build/precache-manifest.f9bd41b93445cf50148386b64cad7dfa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a085fd056284026325fbee260b69e57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36ac33451cc52287a03a",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f3718fd3c0373425a6ff",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36ac33451cc52287a03a",
+    "url": "/static/js/2.f865cf2c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f865cf2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3718fd3c0373425a6ff",
+    "url": "/static/js/main.af0d5e24.chunk.js"
+  },
+  {
+    "revision": "c83d5c2d2a6176bd9825",
+    "url": "/static/js/runtime-main.12d86407.js"
+  }
+]);
